d7667848b607a540c38c686b415191e48a78c6dd
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-10-22 23:30:23 +0100

    update print_array.c function include the header file and some small chnages

--- print_array.c
+++ print_array.c
@@ -1,11 +1,10 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "sort.h"
 
 /**
  * print_array - Prints an array of integers
- *
  * @array: The array to be printed
  * @size: Number of elements in @array
+ * Return: Nothing.
  */
 void print_array(const int *array, size_t size)
 {
@@ -21,3 +20,4 @@ void print_array(const int *array, size_t size)
 	}
 	printf("\n");
 }
+

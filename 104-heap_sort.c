54786cce261e191a0183b20577e4ab92f89e7535
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-10-29 00:21:04 +0100

    update task 8

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -2,59 +2,73 @@
 #include <stdio.h>
 
 /**
- * swap - Swaps two integers
- * @a: First integer
- * @b: Second integer
+ * swap - a function that swaps two integers
+ * @a:  the first integer
+ * @b: the second integer
+ * Return: Nothing
  */
-void swap(int *a, int *b) {
-    int temp = *a;
-    
-    *a = *b;
-    *b = temp;
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
 }
 
 /**
- * heapify - Heapify the array
- * @array: Array to heapify
- * @n: Size of the heap
- * @i: Index of the current node
+ * heapify - a function heapify the array
+ * @array: an array to heapify
+ * @n: the size of the heap
+ * @i: the index of the current node
+ * @size: the size of the array.
+ * Return: Nothing.
  */
-void heapify(int *array, size_t n, size_t i, size_t size) {
-    size_t largest = i;
-    size_t left = 2 * i + 1;
-    size_t right = 2 * i + 2;
+void heapify(int *array, size_t n, size_t i, size_t size)
+{
+	size_t larg = i;
+	size_t left = 2 * i + 1;
+	size_t right = 2 * i + 2;
 
-    if (left < n && array[left] > array[largest])
-        largest = left;
-
-    if (right < n && array[right] > array[largest])
-        largest = right;
-
-    if (largest != i) {
-        swap(&array[i], &array[largest]);
-        print_array(array, size);
-        heapify(array, n, largest, size);
-    }
+	if (left < n && array[left] > array[larg])
+	{
+		larg = left;
+	}
+	if (right < n && array[right] > array[larg])
+	{
+		larg = right;
+	}
+	if (larg != i)
+	{
+		swap(&array[i], &array[larg]);
+		print_array(array, size);
+		heapify(array, n, larg, size);
+	}
 }
 
 /**
- * heap_sort - Sorts an array of integers in ascending order using the Heap sort algorithm
- * @array: Array to sort
- * @size: Size of the array
+ * heap_sort - a function that sorts an array of
+ * integers in ascending
+ * order using the Heap sort algorithm
+ * @array: an array to sort
+ * @size: the size of the array
+ * Return: Nothing.
  */
-void heap_sort(int *array, size_t size) {
-    int i;
+void heap_sort(int *array, size_t size)
+{
+	int x;
 
 	if (size == 0)
-        return;
+		return;
 
-    for (i = size / 2 - 1; i >= 0; i--)
-        heapify(array, size, i, size);
-
-    for (i = size - 1; i > 0; i--) {
-        swap(&array[0], &array[i]);
-        print_array(array, size);
-        heapify(array, i, 0, size);
-    }
+	for (x = size / 2 - 1; x >= 0; x--)
+	{
+		heapify(array, size, x, size);
+	}
+	for (x = size - 1; x > 0; x--)
+	{
+		swap(&array[0], &array[x]);
+		print_array(array, size);
+		heapify(array, x, 0, size);
+	}
 }
 

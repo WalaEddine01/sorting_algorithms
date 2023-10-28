a7d7eb5bc48fac70e78645b78af348e1de667cde
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-10-28 20:24:12 +0100

    complet task 6

--- sort.h
+++ sort.h
@@ -35,6 +35,7 @@ void print_Done(int *array, int size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 #endif
 

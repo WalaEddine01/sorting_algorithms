67481894799b7916822433e11d7ffb38034d0792
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-10-28 19:31:50 +0100

    update task 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,22 +9,24 @@
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, i, j;
+	size_t gap = 1, i, j;
 	int temp;
 
-	for (gap = size/2; gap > 0; gap /= 2)
+	while (gap < size / 3)
 	{
-		for(i = gap; i < size; i += 1)
-		{
-			temp = array[i];
-			
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-			{
-				array[j] = array[j - gap];	
-			}
-			array[j] = temp;
-		}
-		print_array(array, size);
+		gap = gap * 3 + 1;
+	}
+	while (gap > 0)
+	{
+	for (i = gap; i < size; i++)
+	{
+		temp = array[i];
+		for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			array[j] = array[j - gap];
+		array[j] = temp;
+	}
+	print_array(array, size);
+	gap = (gap - 1) / 3;
 	}
 }
 

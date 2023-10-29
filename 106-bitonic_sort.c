45e60c84abb7d6dc361257c731964233986afc48
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-10-29 13:48:32 +0100

    update task 10

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,42 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
-void bitonic_merge(int *arr, size_t n, size_t step, int dir) {
-    if (step > 1) {
-        size_t k = n / 2;
-        for (size_t i = 0; i < k; i++) {
-            if ((arr[i] > arr[i + k]) == dir) {
-                int temp = arr[i];
-                arr[i] = arr[i + k];
-                arr[i + k] = temp;
-                printf("Merging [%lu/%lu] (%s):\n", step, n, dir == 1 ? "UP" : "DOWN");
-                print_array(arr, n);
-            }
-        }
-        bitonic_merge(arr, k, step / 2, dir);
-        bitonic_merge(arr + k, k, step / 2, dir);
-    }
-}
-
 void bitonic_sort(int *array, size_t size) {
-    for (size_t step = 2; step <= size; step *= 2) {
-        for (size_t j = step / 2; j > 0; j /= 2) {
-            for (size_t i = 0; i < size; i++) {
-                size_t k = i ^ j;
-                if (k > i) {
-                    if ((i & step) == 0 && array[i] > array[k]) {
-                        int temp = array[i];
-                        array[i] = array[k];
-                        array[k] = temp;
-                        printf("Merging [%lu/%lu] (%s):\n", step, size, (array[i] > array[k]) ? "UP" : "DOWN");
-                        print_array(array, size);
-                    }
+    if (array == NULL || size < 2) {
+        return;
+    }
+
+    size_t i, j;
+    for (i = 2; i <= size; i *= 2) {
+        for (j = 0; j < size; j += i) {
+            size_t start = j;
+            size_t mid = j + i / 2;
+            size_t end = j + i;
+            size_t k;
+
+            printf("Merging [%zu/%zu] (UP):\n", i, size);
+
+            for (k = start; k < end / 2; k++) {
+                printf("%d, ", array[k]);
+            }
+
+            printf("\n");
+
+            for (k = end / 2; k < end; k++) {
+                printf("%d", array[k]);
+                if (k != end - 1) {
+                    printf(", ");
                 }
             }
-            bitonic_merge(array, size, step, 1);
-            bitonic_merge(array, size, step, 0);
+
+            printf("\n\n");
+
+            for (k = start; k < mid; k++) {
+                if (array[k] > array[k + i / 2]) {
+                    int temp = array[k];
+                    array[k] = array[k + i / 2];
+                    array[k + i / 2] = temp;
+                }
+            }
         }
     }
 }
-
